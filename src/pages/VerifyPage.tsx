dca4ccb09c723e21b957df0ff2e49a071c8ff2cd
gunner <gunnersupanut@gmail.com>
2025-12-28 21:00:16 +0700

    refactor: VerifyPage margin/ br

--- src/pages/VerifyPage.tsx
+++ src/pages/VerifyPage.tsx
@@ -35,8 +35,7 @@ const VerifyPage = () => {
             We have sent a confirmation link to <br />
             <span className="text-secondary">
               {state.email || "Your Email"}
-            </span>
-            <br />
+            </span> 
             <br />
             Please click the link in the email to activate account. <br />
             (Don't forget to check your Spam folder!)
@@ -46,11 +45,11 @@ const VerifyPage = () => {
         <Link
           to="/"
           type="submit"
-          className="w-3/4 mx-auto block mt-20 bg-secondary text-center text-text_inverse text-button py-3 rounded-[20px] shadow-custom hover:bg-yellow-400 transition-transform transform hover:scale-105"
+          className="w-3/4 mx-auto block mt-16 bg-secondary text-center text-text_inverse text-button py-3 rounded-[20px] shadow-custom hover:bg-yellow-400 transition-transform transform hover:scale-105"
         >
           Go to HomePage
         </Link>
-        <div className="text-center mt-24">
+        <div className="text-center mt-14">
           <p className="text-white text-bodyxl">
             Didn't receive the email?{" "}
             <button className="underline text-tertiary text-bodyxl hover:text-secondary">
