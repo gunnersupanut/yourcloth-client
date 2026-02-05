23180bc64f324fd093266847a4c704014239f265
gunner <gunnersupanut@gmail.com>
2026-02-05 20:33:44 +0700

    fix:formateDate Help Func

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,18 +1,18 @@
 export const formatDate = (dateString: string) => {
-    if (!dateString) return "-";
+  if (!dateString) return "-";
 
-    const date = new Date(dateString);
+  const date = new Date(dateString);
 
-    // บวก 7 ชั่วโมงเข้าไปตรงๆ เลย (หน่วย Milliseconds)
-    // 7 ชม. * 60 นาที * 60 วินาที * 1000 มิลลิวินาที
-    const thaiDate = new Date(date.getTime() + 7 * 60 * 60 * 1000);
+  // const thaiDate = new Date(date.getTime() + 7 * 60 * 60 * 1000);
 
-    return thaiDate.toLocaleString("en-GB", {
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-    });
+  // ใช้อันนี้แทน ใส่ options timeZone เข้าไป จบ!
+  return date.toLocaleString("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+    timeZone: "Asia/Bangkok", 
+  });
 };
